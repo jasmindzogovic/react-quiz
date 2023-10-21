5b418891af7810019ffa7df760c9b07d80b9db8f
Jasmin Dzogovic <jasminxhogovic@gmail.com>
2023-10-21 12:03:28 -0400

    added hooks to fetch api

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
+import { useEffect, useReducer } from "react";
 import Header from "./Header";
 import Main from "./Main";
+import Loader from "./Loader";
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, questions: action.payload, status: "ready" };
+    case "dataFailed":
+      return { ...state, status: "error" };
+    default:
+      throw new Error("Uknown action");
+  }
+}
+
+// 'loading', 'error', 'ready', 'active', 'finished'
+const initialState = { questions: [], status: "loading" };
 
 export default function App() {
+  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+  useEffect(() => {
+    fetch("http://localhost:9000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((error) => dispatch({ type: "dataFailed" }));
+  }, []);
   return (
     <div className="app">
       <Header />
 
-      <Main>
-        <p>1/15</p>
-        <p>Question?</p>
-      </Main>
+      <Main>{status === "loading" && <Loader />}
+      {status ==='ready' && }</Main>
     </div>
   );
 }
